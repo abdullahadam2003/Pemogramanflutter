7e2fd03ae22aec9a438fd36e77f907e5221a0f7c
abdullahadam2003 <abdullahadam2017@gmail.com>
2023-12-20 20:34:51 +0700

    nilai

---
+++ flutter/nilai/src/test/java/com/adam/nilai/NilaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adam.nilai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NilaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
